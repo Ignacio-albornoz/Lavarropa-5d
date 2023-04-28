cffad1ddf5b172d9db1c9e73493b521342247984
ignacio-albornoz <IgnacioG.Albornoz@gmail.com>
2023-04-28 11:08:37 -0300

    done

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 
 using namespace std;
 
-//tipoDeVariable nombreVariable = valorInicial
 
 int main(){
     
     int N, LA, LB, SB, SA;
 
+    cout << "Numero de prendas: ";
     cin >> N;
+    cout << "Cantidad de prendas MINIMA y MAXIMA aceptada por el secador: ";
     cin >> LA >> LB;
+    cout << "Cantidad de prendas MINIMA y MAXIMA aceptada por el lavarropas: ";
     cin >> SA >> SB;
 
     if (N >= LA) {
