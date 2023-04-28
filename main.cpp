87a9d638ec3c144a5e71518f1284d3a786593679
ignacio-albornoz <IgnacioG.Albornoz@gmail.com>
2023-04-28 10:42:10 -0300

    se agrego cout descriptivo

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main(){
     
     int N, LA, LB, SB, SA;
-
+    cout << "Numero de prendas: ";
     cin >> N;
+    cout << "Cantidad de prendas MINIMA y MAXIMA aceptada por el secador: ";
     cin >> LA >> LB;
+    cout << "Cantidad de prendas MINIMA y MAXIMA aceptada por el lavarropas: ";
     cin >> SA >> SB;
 
     if(N >= LA && N <= LB && N >= SA && N <= SB ){
